cf3191e67ac70365bab6f339045770d604e950ac
meavv <c.c.c.p@bk.ru>
2021-03-09 00:14:40 +0500

    1.6. Взаимодействие объектов [#431569]

---
+++ src/main/java/ru/job4j/oop/Ball.java
@@ -0,0 +1,14 @@
+package ru.job4j.oop;
+
+public class Ball {
+    public void tryToLeave(Wolf wolf) {
+
+    }
+
+    public void tryToLeave(Hare hare) {
+
+    }
+    public void die(Fox fox) {
+
+    }
+}
