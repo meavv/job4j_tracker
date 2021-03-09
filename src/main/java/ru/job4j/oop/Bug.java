88ae3f214814f216cc0df80e5300809d860b4295
meavv <c.c.c.p@bk.ru>
2021-03-09 12:07:26 +0500

    1.7. Конструктор [#431572]

---
+++ src/main/java/ru/job4j/oop/Bug.java
@@ -0,0 +1,15 @@
+package ru.job4j.oop;
+
+public class Bug {
+    private String name;
+
+    public Bug(String text) {
+        this.name = text;
+    }
+
+    public static void main(String[] args) {
+        String issue = "Fix me ASAP.";
+        Bug item = new Bug(issue);
+
+    }
+}
---
+++ src/main/java/ru/job4j/oop/Fix.java
@@ -0,0 +1,16 @@
+package ru.job4j.oop;
+
+public class Fix {
+    private String desc;
+
+    public Fix(String text) {
+         this.desc = text;
+     }
+
+    public static void main(String[] args) {
+        Fix issue = new Fix("Qwe");
+
+    }
+
+
+}
