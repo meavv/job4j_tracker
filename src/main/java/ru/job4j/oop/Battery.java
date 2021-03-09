084a982e7a037529d87521f9b44b0e64ebfcfb0a
meavv <c.c.c.p@bk.ru>
2021-03-09 14:40:30 +0500

    battery

--- src/main/java/ru/job4j/oop/Battery.java
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -4,8 +4,8 @@ public class Battery {
     private int load;
 
     public void exchange(Battery another) {
-        this.load = this.load + another.load;
-        another.load = 0;
+        another.load = this.load + another.load;
+        this.load = 0;
      }
 
 }
