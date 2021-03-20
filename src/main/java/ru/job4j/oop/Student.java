df0e3290c6eaef8e7e3c759a24b947fb172a0dc2
meavv <c.c.c.p@bk.ru>
2021-03-20 13:49:22 +0500

    up casting

--- src/main/java/ru/job4j/oop/Student.java
+++ src/main/java/ru/job4j/oop/Student.java
@@ -1,18 +1,5 @@
 package ru.job4j.oop;
 
-public class Student {
+public class Student extends  Object{
 
-    public void song() {
-        System.out.println("I believe I can fly");
-    }
-
-    public void music(String lyrics) {
-        System.out.println("I can sign a song : " + lyrics);
-    }
-
-    public static void main(String[] args) {
-        Student petya = new Student();
-       String song = "I believe I can fly";
-       petya.music(song);
-    }
 }
