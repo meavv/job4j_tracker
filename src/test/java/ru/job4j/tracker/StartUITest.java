84a8f983f8cd32b35764fcd36ec6c8f6c0ddaff3
meavv <c.c.c.p@bk.ru>
2021-03-24 13:35:33 +0500

    9.2. Тесты вывода на консоль [#431513]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -67,4 +67,91 @@ public class StartUITest {
                         "0. Exit" + System.lineSeparator()
         ));
     }
+
+    @Test
+    public void findAllActions() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"0", "Q", "1", "2"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new CreateAction(out), new FindAllActions(out), new ExitProgram(out)
+        };
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(out.toString(), is(
+                "Menu." + System.lineSeparator() +
+                        "0. Create" + System.lineSeparator() +
+                        "1. Find all Item" + System.lineSeparator() +
+                        "2. Exit" + System.lineSeparator() +
+                        "=== Create a new Item ====" + System.lineSeparator() +
+                        "Menu." + System.lineSeparator() +
+                        "0. Create" + System.lineSeparator() +
+                        "1. Find all Item" + System.lineSeparator() +
+                        "2. Exit" + System.lineSeparator() +
+                        "Item{id=1, name='Q'}" + System.lineSeparator() +
+                        "Menu." + System.lineSeparator() +
+                        "0. Create" + System.lineSeparator() +
+                        "1. Find all Item" + System.lineSeparator() +
+                        "2. Exit" + System.lineSeparator()
+        ));
+    }
+
+    @Test
+    public void findByName() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"0", "Q", "1", "Q", "2"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new CreateAction(out), new FindByNameActions(out), new ExitProgram(out)
+        };
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(out.toString(), is(
+                "Menu." + System.lineSeparator() +
+                        "0. Create" + System.lineSeparator() +
+                        "1. Find by name" + System.lineSeparator() +
+                        "2. Exit" + System.lineSeparator() +
+                        "=== Create a new Item ====" + System.lineSeparator() +
+                        "Menu." + System.lineSeparator() +
+                        "0. Create" + System.lineSeparator() +
+                        "1. Find by name" + System.lineSeparator() +
+                        "2. Exit" + System.lineSeparator() +
+                        "Item{id=1, name='Q'}" + System.lineSeparator() +
+                        "Menu." + System.lineSeparator() +
+                        "0. Create" + System.lineSeparator() +
+                        "1. Find by name" + System.lineSeparator() +
+                        "2. Exit" + System.lineSeparator()
+        ));
+    }
+
+    @Test
+    public void findById() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"0", "Q", "1", "1", "2"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new CreateAction(out), new FindByIdActions(out), new ExitProgram(out)
+        };
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(out.toString(), is(
+                "Menu." + System.lineSeparator() +
+                        "0. Create" + System.lineSeparator() +
+                        "1. Find by id" + System.lineSeparator() +
+                        "2. Exit" + System.lineSeparator() +
+                        "=== Create a new Item ====" + System.lineSeparator() +
+                        "Menu." + System.lineSeparator() +
+                        "0. Create" + System.lineSeparator() +
+                        "1. Find by id" + System.lineSeparator() +
+                        "2. Exit" + System.lineSeparator() +
+                        "Item{id=1, name='Q'}" + System.lineSeparator() +
+                        "Menu." + System.lineSeparator() +
+                        "0. Create" + System.lineSeparator() +
+                        "1. Find by id" + System.lineSeparator() +
+                        "2. Exit" + System.lineSeparator()
+        ));
+    }
 }
