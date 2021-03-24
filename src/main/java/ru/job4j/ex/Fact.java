6b3175dc2dfb2f3c1ef54e132ef04b4b328d413e
meavv <c.c.c.p@bk.ru>
2021-03-24 15:05:30 +0500

    0.2. Кидаем исключение - throw new RuntimeException [#431534]

---
+++ src/main/java/ru/job4j/ex/Fact.java
@@ -0,0 +1,21 @@
+package ru.job4j.ex;
+
+public class Fact {
+    public static void main(String[] args) {
+        Fact fact = new Fact();
+        int x = fact.calc(-4);
+        System.out.println(x);
+
+    }
+
+    public int calc(int n) {
+        int rsl = 1;
+        if (n < 0) {
+            throw new IllegalArgumentException("Start should be less then finish.");
+        }
+        for (int index = 1; index <= n; index++) {
+            rsl *= index;
+        }
+        return rsl;
+    }
+}
