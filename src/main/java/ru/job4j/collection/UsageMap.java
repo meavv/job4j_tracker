8152dc9ef98b1efb32bf8f5477e48efb7aa06765
meavv <c.c.c.p@bk.ru>
2021-04-17 13:35:38 +0500

    0. Map, HashMap [#431501]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,15 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("c.c.c.p@bk.ru", "Evgebniy Malgin");
+        for (String key : map.keySet()) {
+            String value = map.get(key);
+            System.out.println(key + " = " + value);
+        }
+    }
+}
