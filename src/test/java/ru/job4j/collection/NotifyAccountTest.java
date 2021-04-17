3b05a2203ec7b390c245811cd45175f139a732e0
meavv <c.c.c.p@bk.ru>
2021-04-17 11:13:25 +0500

    3. Модели данных и HashSet. [#431508]

---
+++ src/test/java/ru/job4j/collection/NotifyAccountTest.java
@@ -0,0 +1,58 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class NotifyAccountTest {
+
+    @Test
+    public void sent() {
+        List<Account> accounts = Arrays.asList(
+                new Account("123", "Petr Arsentev", "eDer3432f"),
+                new Account("142", "Petr Arsentev", "000001")
+        );
+        HashSet<Account> expect = new HashSet<>(
+                Arrays.asList(
+                        new Account("123", "Petr Arsentev", "eDer3432f"),
+                        new Account("142", "Petr Arsentev", "000001")
+                )
+        );
+        assertThat(NotifyAccount.sent(accounts), is(expect));
+    }
+
+    @Test
+    public void sent1() {
+        List<Account> accounts = Arrays.asList(
+                new Account("1", "Qwe", "eDer3432f"),
+                new Account("2", "Ewq", "000001"),
+                new Account("2", "Qwe", "3453453")
+        );
+        HashSet<Account> expect = new HashSet<>(
+                Arrays.asList(
+                        new Account("1", "Qwe", "eDer3432f"),
+                        new Account("2", "Ewq", "000001")
+                )
+        );
+        assertThat(NotifyAccount.sent(accounts), is(expect));
+    }
+
+    @Test
+    public void sent2() {
+        List<Account> accounts = Arrays.asList(
+                new Account("555", "Petr Arsentev", "eDer3432f"),
+                new Account("555", "Qwewe", "000001")
+        );
+        HashSet<Account> expect = new HashSet<>(
+                Arrays.asList(
+                        new Account("555", "Petr Arsentev", "eDer3432f")
+                )
+        );
+        assertThat(NotifyAccount.sent(accounts), is(expect));
+    }
+}
