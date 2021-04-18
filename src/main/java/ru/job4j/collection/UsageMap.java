1995c31e75829a59372bc15d20c644afc2d7d24f
meavv <c.c.c.p@bk.ru>
2021-04-18 13:01:13 +0500

    .

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,7 +6,7 @@ import java.util.Map;
 public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
-        map.put("c.c.c.p@bk.ru", "Evgebniy Malgin");
+        map.put("c.c.c.p@bk.ru", "Evgeniy Malgin");
         for (String key : map.keySet()) {
             String value = map.get(key);
             System.out.println(key + " = " + value);
