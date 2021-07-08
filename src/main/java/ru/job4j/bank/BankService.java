0415c5d88cbd4bf166216bcebb9ff6f2cfccacf5
meavv <c.c.c.@bk.ru>
2021-07-08 12:33:36 +0500

    2. Что такое JavaDoc [#313879]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -2,13 +2,31 @@ package ru.job4j.bank;
 
 import java.util.*;
 
+/**
+ * Класс описывает работу различных банковских сервисов
+ * @author Malgin Evgeniy
+ * @version 1.0
+ */
 public class BankService {
+    /**
+     * Используется коллекция Мап
+     * Содержит всех пользователей системы и привязанные к ним счета
+     */
     private Map<User, List<Account>> users = new HashMap<>();
 
+    /**
+     * Метод добавляет польхователя в users
+     * @param user пользователь
+     */
     public void addUser(User user) {
         users.putIfAbsent(user, new ArrayList<Account>());
     }
 
+    /**
+     * Метод добавляет новый счет к пользователю
+     * @param passport паспорт пользователя
+     * @param account счет
+     */
     public void addAccount(String passport, Account account) {
         User user = findByPassport(passport);
         if (user != null) {
@@ -19,6 +37,11 @@ public class BankService {
         }
     }
 
+    /**
+     * Метод ищет пользователья по паспорту
+     * @param passport паспорт пользователя
+     * @return возвращает user по паспорту
+     */
     public User findByPassport(String passport) {
         User rsl = null;
         for (User key : users.keySet()) {
@@ -30,6 +53,12 @@ public class BankService {
         return rsl;
     }
 
+    /**
+     * Метод ищет счет пользователя по реквизитам
+     * @param passport паспорт пользователя
+     * @param requisite реквищиты пользователя
+     * @return возвращает счет пользователя
+     */
     public Account findByRequisite(String passport, String requisite) {
         Account result = null;
         User user = findByPassport(passport);
@@ -45,6 +74,15 @@ public class BankService {
         return result;
     }
 
+    /**
+     * Метод для перечисления денег с одного счёта на другой счёт.
+     * @param srcPassport данные паспорта пользователя с которого будет выполнятся перевод
+     * @param srcRequisite реквизиты пользователя с которого будет выполнятся перевод
+     * @param destPassport реквизиты пользователя куда будет выполнятся перевод
+     * @param destRequisite данные паспорта пользователя куда будет выполнятся перевод
+     * @param amount сумма перевода
+     * @return
+     */
     public boolean transferMoney(String srcPassport, String srcRequisite,
                                  String destPassport, String destRequisite, double amount) {
         Account reqOut = findByRequisite(srcPassport, srcRequisite);
--- src/main/java/ru/job4j/search/PriorityQueue.java
+++ src/main/java/ru/job4j/search/PriorityQueue.java
@@ -3,8 +3,15 @@ package ru.job4j.search;
 import java.util.LinkedList;
 
 public class PriorityQueue {
+
     private LinkedList<Task> tasks = new LinkedList<>();
 
+    /**
+     * Метод принимает на вход заявку и добавляет ее в очередь.
+     * Если встречаются 2 задания с одинаковым приоритетом, то в очереди
+     * они распределяются по принципу FIFO.
+     * @param task задача которая добавляется в очередь
+     */
     public void put(Task task) {
         int index = 0;
         for (Task element : tasks) {
