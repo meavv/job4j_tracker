792d694d721a8b1969e56b39a23225c1b80ba348
meavv <c.c.c.@bk.ru>
2021-07-24 19:50:42 +0500

    0. Stream API улучшения [#58167 #180831]

--- src/main/java/ru/job4j/array/Merge.java
+++ src/main/java/ru/job4j/array/Merge.java
@@ -3,7 +3,6 @@ package ru.job4j.array;
 public class Merge {
     public static int[] merge(int[] left, int[] right) {
         int[] rsl = new int[left.length + right.length];
-        int t;
         for (int i = 0; i < rsl.length; i++) {
             if (i < left.length) {
                 rsl[i] = left[i];
@@ -14,7 +13,7 @@ public class Merge {
         for (int i = 0; i < rsl.length; i++) {
             for (int j = i + 1; j < rsl.length; j++) {
                 if (rsl[j] < rsl[i]) {
-                    t = rsl[i];
+                    int t = rsl[i];
                     rsl[i] = rsl[j];
                     rsl[j] = t;
                 }
